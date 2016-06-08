3ab3ca4c29dd8250d9f687a7337c3f58a6b29045
drew <mcdermotta@carnegiemuseums.org>
2016-06-08 11:05:05 -0400

    Start models

--- LunchPicker/settings.py
+++ LunchPicker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from secrets import secretKey, databaseConfig
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3dwywl89&_cyl55#h)yi-tgeea(0@%35$3*zo%9!5+j+^o1$1f'
+SECRET_KEY = secretKey()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,12 +75,7 @@ WSGI_APPLICATION = 'LunchPicker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = databaseConfig()
 
 
 # Password validation
--- picker/models.py
+++ picker/models.py
@@ -1,5 +1,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Restaurant(models.Model):
+    name            = models.CharField(max_length=255)
+    description     = models.TextField(blank=True)
+    address         = models.CharField(max_length=255, blank = True)
+    active          = models.BooleanField(default=True)
+
+class History(models.Model):
+    restaurant      = models.ForeignKey(Restaurant, on_delete=models.PROTECT)
+    date            = models.DateField(auto_now_add = True)
+
+class Votes(models.Model):
+    user            = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant      = models.ForeignKey(Restaurant, on_delete=models.SET_NULL, null=True)
+    veto            = models.BooleanField(default=false)
+    timestamp       = models.DateTimeField(auto_now_add = True)
