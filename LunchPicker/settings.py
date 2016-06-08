b4294aa1aef48a83beec7a72c436ad1b8ee7cd9e
drew <mcdermotta@carnegiemuseums.org>
2016-06-08 11:07:18 -0400

    Rename

--- LunchPicker/settings.py
+++ LunchPicker/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-from secrets import secretKey, databaseConfig
+from secret import secretKey, databaseConfig
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
