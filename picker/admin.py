81ada7b671baaa75f3fa0bb8812f3a9565bd7c43
drew <mcdermotta@carnegiemuseums.org>
2016-06-08 16:14:17 -0400

    fix import

--- picker/admin.py
+++ picker/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from mysite.books.models import *
+from picker.models import *
 # Register your models here.
 admin.site.register(Restaurant)
 admin.site.register(History)
