2319705a7de98aba619c5ce2ccfd36a8267ae89b
drew <mcdermotta@carnegiemuseums.org>
2016-06-08 15:21:55 -0400

    Update to real password reset

--- picker/views.py
+++ picker/views.py
@@ -21,9 +21,9 @@ def register(request):
 
 
 
-def reset_confirm(request, uidb64=None, token=None):
+def reset_confirm(request, user=None, token=None):
     return password_reset_confirm(request, template_name='registration/reset/password_reset_confirm.html',
-        uidb36=uidb36, token=token, post_reset_redirect=reverse('login'))
+        uidb64=user, token=token, post_reset_redirect=reverse('login'))
 
 
 def reset(request):
@@ -35,4 +35,4 @@ def reset(request):
 
 # from django.contrib.auth.forms import PasswordResetForm
 # p = PasswordResetForm()
-# p.send_mail(to_email='drew@drewmcdermott.net', email_template_name='registration/reset/password_reset_email.html', subject_template_name='registration/reset/password_reset_subject.txt', context={}, from_email='cmp.lunchpicker@gmail.com')
+# p.send_mail(to_email='drew@drewmcdermott.net', email_template_name='registration/reset/password_reset_email_test.html', subject_template_name='registration/reset/password_reset_subject.txt', context={}, from_email='cmp.lunchpicker@gmail.com')
