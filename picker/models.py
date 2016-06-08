986cfad5b52d251439645be1aa2c7272c986980b
drew <mcdermotta@carnegiemuseums.org>
2016-06-08 11:15:32 -0400

    Fix model

--- picker/models.py
+++ picker/models.py
@@ -13,10 +13,10 @@ class Restaurant(models.Model):
 
 class History(models.Model):
     restaurant      = models.ForeignKey(Restaurant, on_delete=models.PROTECT)
-    date            = models.DateField(auto_now_add = True)
+    date            = models.DateField(auto_now_add=True)
 
 class Votes(models.Model):
     user            = models.ForeignKey(User, on_delete=models.CASCADE)
     restaurant      = models.ForeignKey(Restaurant, on_delete=models.SET_NULL, null=True)
-    veto            = models.BooleanField(default=false)
-    timestamp       = models.DateTimeField(auto_now_add = True)
+    veto            = models.BooleanField(default=False)
+    timestamp       = models.DateTimeField(auto_now_add=True)
